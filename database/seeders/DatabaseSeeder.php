b736bd87254b971606c00e77cbb932510d18fdc5
Wanderson Passos Barcelos <wp.barcelos@gmail.com>
2023-10-05 22:15:06 -0300

    db:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1)->create([
+            'name'=>'Wanderson Passos Barcelos',
+            'email'=>'wp.barcelos@gmail.com',
+            'password'=> bcrypt('password')
+        ]);
+
+        \App\Models\Event::factory(rand(5,10))->create()->each(function($event){
+
+            \App\Models\EventSubscribe::factory(rand(10,40))->create([
+                'event_id'=> $event->id
+            ]);
+
+        });
+
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
